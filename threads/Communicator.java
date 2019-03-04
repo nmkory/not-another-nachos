844b03a4918fb3ba809a4e2865a0352303011753
Nicholas Kory <nmkory@ucdavis.edu>
2019-03-03 17:58:13 -0800

    Beautified text and added comment re cond2 variables todo

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -3,6 +3,8 @@ package nachos.threads;
 import nachos.machine.*;
 
 /**
+ * [TODO] !!!!CHANGE Condition VARIABLES TO Condition2 VARIABLES!!!!
+ * 
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
  * messages. Multiple threads can be waiting to <i>speak</i>,
  * and multiple threads can be waiting to <i>listen</i>. But there should never
@@ -10,83 +12,83 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
-    /**
-     * Allocate a new communicator.
-     */
-    public Communicator() {
-    }
+	/**
+	 * Allocate a new communicator.
+	 */
+	public Communicator() {
+	}
 
-    /**
-     * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
-     *
-     * <p>
-     * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
-     *
-     * @param	word	the integer to transfer.
-     */
-    public void speak(int word) {
-    	boolean intStatus = Machine.interrupt().disable();
-	    lock.acquire();
-	    
-	    while (wordToBeHeard) {
-	    	listener.wake();
-	    	speaker.sleep();
-	    }
-	    
-	   this.word = word;
-	   wordToBeHeard = true;
-	   listener.wake();
-	   speaker.sleep();
-	   lock.release();
-	   Machine.interrupt().restore(intStatus);
-    }  //speak()
-    
-    /**
-     * Wait for a thread to speak through this communicator, and then return
-     * the <i>word</i> that thread passed to <tt>speak()</tt>.
-     *
-     * @return	the integer transferred.
-     */    
-    public int listen() {
-	    boolean intStatus = Machine.interrupt().disable();
-	    lock.acquire();
-	    
-	    while (!wordToBeHeard) {
-	    	speaker.wake();
-	    	listener.sleep();	
-	    }
-	    
-	   int wordToHear = word;
-	   wordToBeHeard = false;
-	   speaker.wake();
-	   lock.release();
-	   Machine.interrupt().restore(intStatus);
-	   return wordToHear; 	
-   }  //listen()   	
-    
-    
-    
-/*    private static class speakerRun implements Runnable {
+	/**
+	 * Wait for a thread to listen through this communicator, and then transfer
+	 * <i>word</i> to the listener.
+	 *
+	 * <p>
+	 * Does not return until this thread is paired up with a listening thread.
+	 * Exactly one listener should receive <i>word</i>.
+	 *
+	 * @param	word	the integer to transfer.
+	 */
+	public void speak(int word) {
+		boolean intStatus = Machine.interrupt().disable();
+		lock.acquire();
+
+		while (wordToBeHeard) {
+			listener.wake();
+			speaker.sleep();
+		}
+
+		this.word = word;
+		wordToBeHeard = true;
+		listener.wake();
+		speaker.sleep();
+		lock.release();
+		Machine.interrupt().restore(intStatus);
+	}  //speak()
+
+	/**
+	 * Wait for a thread to speak through this communicator, and then return
+	 * the <i>word</i> that thread passed to <tt>speak()</tt>.
+	 *
+	 * @return	the integer transferred.
+	 */    
+	public int listen() {
+		boolean intStatus = Machine.interrupt().disable();
+		lock.acquire();
+
+		while (!wordToBeHeard) {
+			speaker.wake();
+			listener.sleep();	
+		}
+
+		int wordToHear = word;
+		wordToBeHeard = false;
+		speaker.wake();
+		lock.release();
+		Machine.interrupt().restore(intStatus);
+		return wordToHear; 	
+	}  //listen()   	
+
+
+
+	/*    private static class speakerRun implements Runnable {
     	speakerRun(Communicator myCom) {
     	    this.myCom = myCom;
     	}
-    	
+
     	public void run() {
     	   myCom.speak(2);    	    
     	}
 
     	private Communicator myCom;
         }  //speakerRun()
-    
+
     private static class listenRun implements Runnable {
     	listenRun(Communicator myCom) {
     	    this.myCom = myCom;
     	    Lib.debug(dbgThread,
       			  	"Creating listen thread");
     	}
-    	
+
     	public void run() {
     		Lib.debug(dbgThread,
       			  	"Thread " + KThread.currentThread().getName() + "is about to listen");
@@ -97,29 +99,29 @@ public class Communicator {
     	private Communicator myCom;
         }  //listenerRun()
 
-        
+
         public static void selfTest1() {
     	Communicator testCom = new Communicator();
 
     	KThread listener1 = new KThread(new listenRun(testCom));
     	listener1.setName("listener1");
     	listener1.fork();
-    	
+
     	KThread speaker1 = new KThread(new speakerRun(testCom));
     	speaker1.setName("speaker1");
     	speaker1.fork();
-    	
-    	
+
+
         }  //selfTest1()
 
     private static final char dbgThread = 't';
-*/
-    
-    private boolean wordToBeHeard = false;
-    private int word;
-    private Lock lock = new Lock();
-    private Condition listener =
-    		new Condition (lock);
-    private Condition speaker =
-    		new Condition (lock);
+	 */
+
+	private boolean wordToBeHeard = false;
+	private int word;
+	private Lock lock = new Lock();
+	private Condition listener =
+			new Condition (lock);
+	private Condition speaker =
+			new Condition (lock);
 }
